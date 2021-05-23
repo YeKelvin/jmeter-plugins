2dc34de2089110886197b3cf76203cd1d46fdcfc
Kelvin.Ye <testmankelvin@163.com>
2021-05-23 17:17:49 +0800

    fix: macOS终于可以在GUI脚本中打开脚本了

--- common-util/src/main/java/org/apache/jmeter/common/utils/RuntimeUtil.java
+++ common-util/src/main/java/org/apache/jmeter/common/utils/RuntimeUtil.java
@@ -1,6 +1,7 @@
 package org.apache.jmeter.common.utils;
 
 import com.google.common.base.Joiner;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,13 +16,13 @@ public class RuntimeUtil {
 
     private static final Logger log = LoggerFactory.getLogger(RuntimeUtil.class);
 
-    private static final String WINDOWS = "windows";
-    private static final String MAC = "mac";
-    private static final String LINUX = "linux";
+    private static final String OS_WINDOWS = "windows";
+    private static final String OS_MAC = "mac";
+    private static final String OS_LINUX = "linux";
 
-    private static final String WIN_CMD = "c:\\Windows\\System32\\cmd.exe";
-    private static final String MAC_TERMINAL = "/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal";
-    private static final String MAC_ITERM2 = "/Applications/iTerm.app/Contents/MacOS/iTerm2";
+    private static final String CMD = "c:\\Windows\\System32\\cmd.exe";
+    private static final String TERMINAL = "/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal";
+    private static final String ITERM2 = "/Applications/iTerm.app/Contents/MacOS/iTerm2";
 
 
     public static void exec(String command, String dirPath) throws IOException {
@@ -30,11 +31,11 @@ public class RuntimeUtil {
 
     public static void exec(String command, File dir) throws IOException {
         String osName = System.getProperty("os.name");
-        if (osName.toLowerCase().contains(WINDOWS)) {
+        if (osName.toLowerCase().contains(OS_WINDOWS)) {
             exec4Windows(command, dir);
-        } else if (osName.toLowerCase().contains(MAC)) {
+        } else if (osName.toLowerCase().contains(OS_MAC)) {
             exec4Mac(command, dir);
-        }else if (osName.toLowerCase().contains(LINUX)) {
+        } else if (osName.toLowerCase().contains(OS_LINUX)) {
             exec4Linux(command, dir);
         }
     }
@@ -45,22 +46,43 @@ public class RuntimeUtil {
      * cmd /c start dir 会打开一个新窗口后执行dir指令，原窗口会关闭
      * cmd /k start dir 会打开一个新窗口后执行dir指令，原窗口不会关闭
      */
-    private static void exec4Windows(String command, File dir) throws IOException {
-        String[] fullCommand = new String[]{"cmd", "/c", "start " + command};
-        log.info("workspace:[ {} ] execute:[ {} ]", dir.getPath(), Joiner.on(" ").join(fullCommand));
-        Runtime.getRuntime().exec(fullCommand, null, dir);
+    public static void exec4Windows(String command, String dir) throws IOException {
+        exec4Windows(command, new File(dir));
     }
 
-    private static void exec4Mac(String command, File dir) throws IOException {
-        String[] fullCommand = new String[]{MAC_ITERM2, command};
-        log.info("workspace:[ {} ] execute:[ {} ]", dir.getPath(), Joiner.on(" ").join(fullCommand));
-        Runtime.getRuntime().exec(fullCommand, null, dir);
+    public static void exec4Windows(String command, File dir) throws IOException {
+        String[] fullCmd = new String[]{"cmd", "/c", "start " + command};
+        log.info("workspace:[ {} ] execute:[ {} ]", dir.getPath(), Joiner.on(" ").join(fullCmd));
+        Runtime.getRuntime().exec(fullCmd, null, dir);
     }
 
-    private static void exec4Linux(String command, File dir) throws IOException {
-        String[] fullCommand = new String[]{"/bin/sh", "-c", "xterm -e " + command};
-        log.info("workspace:[ {} ] execute:[ {} ]", dir.getPath(), Joiner.on(" ").join(fullCommand));
-        Runtime.getRuntime().exec(fullCommand, null, dir);
+    public static void exec4Mac(String command, String dir) throws IOException {
+        exec4Mac(command, new File(dir));
+    }
+
+    public static void exec4Mac(String command, File dir) throws IOException {
+        String[] cmdArray = command.split(" ");
+        exec4Mac(cmdArray, dir);
+    }
+
+    public static void exec4Mac(String[] cmdArray, String dir) throws IOException {
+        exec4Mac(cmdArray, new File(dir));
+    }
+
+    public static void exec4Mac(String[] cmdArray, File dir) throws IOException {
+        String[] fullCmd = ArrayUtils.addAll(new String[]{"/bin/sh"}, cmdArray);
+        log.info("workspace:[ {} ] execute:[ {} ]", dir.getPath(), Joiner.on(" ").join(fullCmd));
+        Runtime.getRuntime().exec(fullCmd, null, dir);
+    }
+
+    public static void exec4Linux(String command, String dir) throws IOException {
+        exec4Linux(command, new File(dir));
+    }
+
+    public static void exec4Linux(String command, File dir) throws IOException {
+        String[] fullCmd = new String[]{"/bin/sh", "-c", "xterm -e " + command};
+        log.info("workspace:[ {} ] execute:[ {} ]", dir.getPath(), Joiner.on(" ").join(fullCmd));
+        Runtime.getRuntime().exec(fullCmd, null, dir);
     }
 
 }
